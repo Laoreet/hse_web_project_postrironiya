a2434a29dfb245d256c1f8a6b7a567345acec96a
Sergey Kiryanov <aappkkaa@gmail.com>
2024-06-10 03:51:23 +0500

    Тест коммит ветки

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 
 function Navbar() {
+  // тест коммит светки
   const [isAuthenticated, setIsAuthenticated] = useState(false);
 
   useEffect(() => {
