b015033649ac4afd4fd90b9cba571df1cf50df16
mikisheva.p@gmail.com <mikisheva.p@gmail.com>
2024-06-21 22:19:43 +0500

    Красивый вывод на странице Слоты

--- src/components/Slots.tsx
+++ src/components/Slots.tsx
@@ -22,8 +22,30 @@ const Dashboard: React.FC = () => {
   const [slots, setSlots] = useState<ISlot[]>([]);
   const [users, setUsers] = useState<IUser[]>([]);
   const [wms, setWms] = useState<IWM[]>([]);
+  const [userNameMap, setUserNameMap] = useState<Record<string, string>>({});
 
   const dbRef = ref(db);
+
+  useEffect(() => {
+    const usersRef = ref(db, 'Users');
+    onValue(usersRef, (snapshot) => {
+      const userData = snapshot.val();
+      const usersList: IUser[] = [];
+      const userNameMap: { [id: string]: string } = {};
+      for (let id in userData) {
+        if (userData.hasOwnProperty(id)) {
+          usersList.push({ id, ...userData[id] });
+          userNameMap[id] = userData[id].first_name; // Store user ID to name mapping
+        }
+      }
+      setUsers(usersList);
+      setUserNameMap(userNameMap);
+    });
+
+    return () => {
+      off(usersRef);
+    };
+  }, []);
 
   useEffect(() => {
     const slotsRef = ref(db, 'Slots');
@@ -39,26 +61,43 @@ const Dashboard: React.FC = () => {
     });
 
     return () => {
-        off(slotsRef);
+      off(slotsRef);
     };
   }, []);
+
+
 
   // Render the data here
 
   return (
-    <div className='container'>
-      <h1>Слоты</h1>
-      <div className='dashboard'>
-        <h2>Список слотов</h2>
-        <ul>
-          {slots.map((slot) => (
-            <li key={slot.id}>
-              {slot.start} - {slot.user_id} - Стиралка номер {slot.wm_id}
-            </li>
-          ))}
-        </ul>
+      <div className='container'>
+        <h1>Слоты</h1>
+        <div className='dashboard'>
+          <table>
+            <caption className="caption-large">Список слотов</caption>
+            <thead>
+            <tr>
+              <th>Время начала</th>
+              <th>Имя</th>
+              <th>Фамилия</th>
+              <th>Email</th>
+              <th>Стиралка номер</th>
+            </tr>
+            </thead>
+            <tbody>
+            {slots.map((slot, index) => (
+                <tr key={slot.id} className={index % 2 === 0 ? 'even' : 'odd'}>
+                  <td>{slot.start}</td>
+                  <td>{userNameMap[slot.user_id]}</td>
+                  <td>{users.find((user) => user.id === slot.user_id)?.last_name}</td>
+                  <td>{slot.user_id}</td>
+                  <td>Стиралка номер {slot.wm_id}</td>
+                </tr>
+            ))}
+            </tbody>
+          </table>
+        </div>
       </div>
-    </div>
   );
 };
 
